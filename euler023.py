b07b12b2645e394c5a85a3585512acf7bdfb05b7
Andrea Agosti <cifvts@gmail.com>
2011-01-23 14:47:13 +0100

    Add solution for problem 23, it's quite fast

---
+++ euler023.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+from math import floor, sqrt
+
+LIMIT = 28134
+
+
+def isAbundant(n):
+    max_t = floor(sqrt(n)) + 1
+    sum_d = 1
+    for i in range(2, max_t):
+        if n % i == 0:
+            sum_d += i + n // i
+        if i == n / i:
+            sum_d -= i
+    return sum_d > n
+
+""" Main """
+abd_l = [0] * LIMIT
+for i in range(12, LIMIT):
+    if isAbundant(i):
+        abd_l[i] = 1
+
+sum_abd = 0
+for i in range(1, LIMIT):
+    abd = 0
+    for j in range(12, (i // 2) + 1):
+        if abd_l[j] and abd_l[i - j]:
+            abd = 1
+            break
+    if not abd:
+        sum_abd += i
+
+print(sum_abd)
