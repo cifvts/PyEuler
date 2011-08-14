9f648ac80ee01709c946561c90c83575a4263c79
Andrea Agosti <cifvts@gmail.com>
2011-08-14 15:37:19 +0200

    Fix indentation and PEP8

--- euler036.py
+++ euler036.py
@@ -5,20 +5,22 @@ from math import pow
 LIMIT = 1000000
 palindrome_sum = 0
 
+
 def is_palindrome(res):
-	return res == res[::-1]
+    return res == res[::-1]
+
 
 def binary(x):
-	res = []
-	while(x):
-		res.insert(0, x % 2)
-		x //= 2
-	return res
+    res = []
+    while(x):
+        res.insert(0, x % 2)
+        x //= 2
+    return res
 
 for palindrome in range(1, LIMIT):
-	if(is_palindrome(list(str(palindrome)))):
-		binary_n = binary(palindrome)
-		if(is_palindrome(binary_n)):
-			palindrome_sum += palindrome 
+    if(is_palindrome(list(str(palindrome)))):
+        binary_n = binary(palindrome)
+        if(is_palindrome(binary_n)):
+            palindrome_sum += palindrome
 
 print(palindrome_sum)
